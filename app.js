0ad759981143824732c0fe8d67f8e47fd3b3cdb1
Febin Skariya <31155921+febin123@users.noreply.github.com>
2021-10-21 22:41:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function letsSpin() {
+  
+    var x = 1024; //min value
+    var y = 9999; //max value
+
+    var deg = 1800;
+
+    document.getElementById('wheel').style.transform = "rotate("+deg+"deg)";
+  
+}
